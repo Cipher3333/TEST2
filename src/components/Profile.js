ab10918d5542c0c48aa324053601e53ff19ec4f5
Black Reaper <84635243+BlackReaper33@users.noreply.github.com>
2023-02-23 17:43:37 +0000

    Update Profile.js

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,27 +3,31 @@ import { useParams } from "react-router-dom";
 import firebase from 'firebase/app';
 
 const Profile = () => {
-  const { username } = useParams();
+  const { username, uid } = useParams();
   const [userData, setUserData] = useState(null);
 
   useEffect(() => {
     const db = firebase.database();
-    const userRef = db.ref("users/" + username);
-
+    const userRef = db.ref(`users/${uid}/username`);
+  
     userRef.on("value", (snapshot) => {
       const data = snapshot.val();
       if (data) {
         setUserData(data);
       } else {
         const user = firebase.auth().currentUser;
-        setUserData({ username: user.displayName, userId: user.uid });
+        if (user) {
+          setUserData({ username: user.displayName, userId: user.uid });
+        } else {
+          setUserData(null); // or handle the case when there is no authenticated user differently
+        }
       }
     });
-
+  
     return () => {
       userRef.off();
     };
-  }, [username]);
+  }, [username, uid]);  
 
   if (!userData) {
     return <div>Loading...</div>;
